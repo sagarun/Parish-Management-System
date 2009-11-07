1df6ea2958927ec100ec6501915fb3f9bd65cabc
Arun SAG <sagarun@gmail.com>
2009-11-07 16:41:00 +0530

    Validation functions added and GUI button label changed

--- parish_callbacks.c
+++ parish_callbacks.c
@@ -39,6 +39,36 @@ void on_window_baptism_quit_clicked(GtkButton *button,gpointer user_data)
 
 void on_window_baptism_clear_clicked(GtkButton *button,gpointer user_data)
 {
+  	GtkWidget *entry;
+	GtkWidget *cbox;
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_kept_at");
+	gtk_entry_set_text(GTK_ENTRY (entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_place");
+	gtk_entry_set_text(GTK_ENTRY (entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_date");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_name");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_dob");
+	gtk_entry_set_text(GTK_ENTRY (entry),"");
+	cbox = (GtkComboBox *)gtk_builder_get_object(build,"window_baptism_sex");
+	gtk_combo_box_set_active((GtkComboBox *)(cbox),-1);
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_mom");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_dad");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_resi");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_caste");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_sp1");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_sp2");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_priest");
+        gtk_entry_set_text(GTK_ENTRY(entry),"");
+	entry = (GtkWidget *)gtk_builder_get_object(build,"window_baptism_remarks");
+	gtk_entry_set_text(GTK_ENTRY(entry),"");
 		
 }
 
@@ -82,8 +112,245 @@ void on_window_baptism_add_clicked(GtkButton *button,gpointer user_data)
 {
   int retval;
   get_baptism_cert();
+  if(!is_form_valid())
+    {
+      quick_message("None of the fileds should be empty");
+      return;
+    }
   retval = sqlite_store_baptism_cert();
-  g_print("Return value after insert query: %d",retval);
+}
+
+void on_window_baptism_remarks_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+  if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
 
 }
 
+void on_window_baptism_kept_at_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_place_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_name_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_mom_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_dad_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_priest_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_sp1_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_sp2_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s .]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_caste_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[a-zA-Z \s]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_resi_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+
+   if(!g_regex_match_simple("[0-9a-zA-Z \s . ,]+",new_text,0,0))
+    {
+      g_signal_stop_emission_by_name (editable,"insert-text");
+    }
+      
+
+}
+
+void on_window_baptism_dob_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+	if(!g_regex_match_simple("[0-9/]+",new_text,0,0))
+	{
+		g_signal_stop_emission_by_name(editable,"insert-text");
+	}
+}
+void on_window_baptism_date_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data)
+{
+	if(!g_regex_match_simple("[0-9/]+",new_text,0,0))
+	{
+		g_signal_stop_emission_by_name(editable,"insert-text");
+	}
+}
+
+gboolean on_window_baptism_dob_focus_out_event(GtkWidget *widget,GdkEventFocus *event,gpointer user_data)
+{
+	if(!g_regex_match_simple("[0-9]{1,2}[/][0-9]{1,2}[/][0-9]{4}",gtk_entry_get_text((GtkEntry *)widget),0,0))
+	{
+		quick_message("Invalid Date entry in Date of birth field");
+	}
+	return FALSE;
+}
+
+gboolean on_window_baptism_date_focus_out_event(GtkWidget *widget,GdkEventFocus *event,gpointer user_data)
+{
+	if(!g_regex_match_simple("[0-9]{1,2}[/][0-9]{1,2}[/][0-9]{4}",gtk_entry_get_text((GtkEntry *)widget),0,0))
+	{
+		quick_message("Invalid Date entry in Baptism Date filed");
+	}
+	return FALSE;
+}
+
+
+
+/*Utility functions*/
+gboolean is_form_valid()
+{
+  if(g_strcmp0(cert.kept_at,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.place,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.date_baptism,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.name,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.dob,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.mom,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.dad,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.resi,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.caste,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.spon1,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.spon2,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.priest,"")==0)
+    {
+      return FALSE;
+    }
+  else if(g_strcmp0(cert.remarks,"")==0)
+    {
+      return FALSE;
+    }
+  return TRUE;
+  
+}
+
+
+void quick_message (gchar *message) 
+{
+   	GtkWidget *dialog, *label, *content_area,*parent;
+	parent =(GtkWidget *)gtk_builder_get_object(build,"window_baptism");
+   /* Create the widgets */
+  	 dialog = gtk_dialog_new_with_buttons ("Message",
+					 parent,
+                                         GTK_DIALOG_MODAL,
+                                         GTK_STOCK_OK,
+                                         GTK_RESPONSE_OK,
+                                         NULL);
+   	content_area = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
+   	label = gtk_label_new (message);
+   	/* Ensure that the dialog box is destroyed when the user responds. */
+   	g_signal_connect_swapped (dialog,"response",G_CALLBACK (gtk_widget_destroy),dialog);
+   	/* Add the label, and show everything we've added to the dialog. */
+   	gtk_container_add (GTK_CONTAINER (content_area), label);
+  	gtk_widget_show_all (dialog);
+}
--- parish_callbacks.h
+++ parish_callbacks.h
@@ -34,11 +34,27 @@ void on_window_button_marriage_clicked(GtkButton *button,gpointer user_data);
 void on_window_baptism_quit_clicked(GtkButton *button,gpointer user_data);
 void on_window_baptism_clear_clicked(GtkButton *button,gpointer user_data);
 void on_window_baptism_add_clicked(GtkButton *button,gpointer user_data);
+void on_window_baptism_remarks_insert_text(GtkEditable  *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_kept_at_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_place_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_dad_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_mom_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_sp2_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_sp1_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_priest_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_name_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_resi_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_caste_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_dob_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+void on_window_baptism_date_insert_text(GtkEditable *editable,gchar *new_text,gint new_text_length,gint *position,gpointer user_data);
+gboolean on_window_baptism_dob_focus_out_event(GtkWidget *widget,GdkEventFocus *event,gpointer user_data);
+gboolean on_window_baptism_date_focus_out_event(GtkWidget *widget,GdkEventFocus *event,gpointer user_data);
 
 /*utility functions*/
 
 void add_text_window_baptism_sex();
-
+void quick_message(gchar *);
+gboolean is_form_valid();
 
 /*Function related to sqlite database intefacing*/
 int sqlite_get_handle();
